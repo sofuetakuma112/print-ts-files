d04d281330e294de1e5ea24e91c636551597302c
sofuetakuma <kawahagi0620@gmail.com>
2024-11-08 18:01:46 +0900

    npxコマンドが実行できないバグを修正

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,2 +1,3 @@
+#!/usr/bin/env node
 export declare function tryResolveImport(basePath: string, moduleSpecifier: string): string | null;
 export declare function printFileContents(filePath: string, visited?: Set<string>): void;
